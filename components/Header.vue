183fee91ffb72028829fdd2be363ff59dcee6bad
km1y <kmly267@gmail.com>
2020-12-30 21:18:51 +0900

    add: header component

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+<header class="text-gray-400 bg-gray-900 body-font">
+  <div class="origin-continer flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <nuxt-link to="/" class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+      <span class="ml-3 text-xl">km1y.com</span>
+    </nuxt-link>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nuxt-link to="/" class="mr-5 hover:text-white">Home</nuxt-link>
+      <nuxt-link to="/posts/" class="mr-5 hover:text-white">Blog</nuxt-link>
+    </nav>
+  </div>
+</header>
+</template>
+
+<style scoped>
+.origin-continer{
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
