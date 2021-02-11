8f4bcc843b1eef95e88b1d148014991cc2d62508
km1y <kmly267@gmail.com>
2021-02-11 23:08:56 +0900

    fix:  margin layout at posts

--- components/Card.vue
+++ components/Card.vue
@@ -10,7 +10,7 @@
           <h2 class="text-2xl font-medium font-semibold text-white title-font mb-2">{{ post.title }}</h2>
           <!-- <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p> -->
           <p class="leading-relaxed">{{ post.description }}</p>
-          <a class="text-blue-400 inline-flex items-center mt-4">Learn More
+          <a class="my-8 text-blue-400 inline-flex items-center mt-4">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
